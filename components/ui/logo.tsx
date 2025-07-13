52a02efd46998ca1118cf4433b639e1b1e3e370d
Armand PETIT <armand_petit@outlook.fr>
2025-07-13 03:03:50 +0200

    refactor: update logo component and styles; replace custom logo with Swords icon

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Swords } from 'lucide-react';
 
 import { AppInfo } from '@/constants/app-info';
 import { cn } from '@/lib/utils';
@@ -20,30 +21,13 @@ export function Logo({
 }: LogoProps): React.JSX.Element {
   return (
     <div
-      className={cn('flex items-center space-x-2', className)}
+      className={cn('flex items-center space-x-1', className)}
       {...other}
     >
       {!hideSymbol && (
         <div className="flex size-9 items-center justify-center p-1">
-          <div className="flex size-7 items-center justify-center rounded-md border text-primary">
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g>
-                <path
-                  d="M7.81815 8.36373L12 0L24 24H15.2809L7.81815 8.36373Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M4.32142 15.3572L8.44635 24H-1.14809e-06L4.32142 15.3572Z"
-                  fill="currentColor"
-                />
-              </g>
-            </svg>
+          <div className="flex size-9 items-center justify-center text-primary">
+            <Swords className="size-6" />
           </div>
         </div>
       )}
